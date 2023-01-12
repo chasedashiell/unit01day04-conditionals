4b1a82c8cdf621912557b8829b4a3d55153ff8d5
chasedashiell <dashchax@gmail.com>
2023-01-12 12:26:22 -0600

    finished 2.0

--- Quadratic.java
+++ Quadratic.java
@@ -4,14 +4,14 @@ public class Quadratic {
         double ad = a + 0.0;
         double bd = b + 0.0; 
         double cd = c + 0.0; 
-        double disc = bd * bd - 4 * ad * cd;
+        double disc = b * b - 4 * a * c;
   // if the discriminate is less that 0 
       if (disc < 0 ){
         System.out.println("No real roots");
   // if the discriminate == 0 then there is one real root
       } else if (disc == 0){
-        double result1 = (-bd + Math.sqrt(disc)) / (2 * ad);
-        double result2 = (-bd - Math.sqrt(disc)) / (2 * ad);
+        double result1 = (-b + Math.sqrt(disc)) / (2 * a);
+        double result2 = (-b - Math.sqrt(disc)) / (2 * a);
         System.out.println(result1);
         System.out.println(result2);
       }else if (a == 0){
@@ -19,8 +19,8 @@ public class Quadratic {
         System.out.println(result);
       } else {
   // 2 real roots
-        double root1 = (-bd + Math.sqrt(disc)) / (2 * ad);
-        double root2 = (-bd - Math.sqrt(disc)) / (2 * ad);
+        double root1 = (-b + Math.sqrt(disc)) / (2 * a);
+        double root2 = (-b - Math.sqrt(disc)) / (2 * a);
         System.out.println(root1);
         System.out.println(root2);
 
