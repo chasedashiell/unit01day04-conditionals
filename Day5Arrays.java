84ea1099f3e50eae18919d1891cdccfa06bed48a
chasedashiell <dashchax@gmail.com>
2023-01-17 11:35:14 -0600

    arrays1

---
+++ Day5Arrays.java
@@ -0,0 +1,10 @@
+public class Day5Arrays {
+    public static void main(String[] args) {
+        int[] a = {2, 1, 2, 3, 6};
+        if(a[0] == 6 || a[a.length - 1] == 6){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
