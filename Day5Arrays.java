6e542da4aae1960bc4fe5027de18ccd7f3102eab
chasedashiell <dashchax@gmail.com>
2023-01-17 11:41:13 -0600

    Arrays2

--- Day5Arrays.java
+++ Day5Arrays.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
 public class Day5Arrays {
     public static void main(String[] args) {
-        int[] a = {2, 1, 2, 3, 6};
-        if(a[0] == 6 || a[a.length - 1] == 6){
+        int[] nums = {1, 2, 3};
+        if(nums[0] == 2 || nums[1] == 3){
+            nums[1] = 0;
             System.out.println(true);
-        } else {
-            System.out.println(false);
+        } else if(nums[1] == 2 && nums[2] == 3) {
+            nums[2] = 0;
         }
+        System.out.println(Arrays.toString(nums));
     }
 }
